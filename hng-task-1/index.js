538cd745518b22ad6064c2c84783a38f17dbe8c9
Solomon Adzape <adzapesolomon@gmail.com>
2024-07-02 18:28:26 +0100

    feat:add all features

--- hng-task-1/index.js
+++ hng-task-1/index.js
@@ -1,9 +1,24 @@
-/**
- * Project generated with startease - https://startease.vercel.app
- * @link https://startease.vercel.app
- */
+const bannerPic = document.getElementById("banner-pic");
+const p1 = bannerPic.offsetLeft;
+const p2 = bannerPic.offsetRight;
+const width = bannerPic.width;
+let posX, posY;
+function isMe(x) {
+  let p = ((x - p1) / width) * 100;
+  bannerPic.title =
+    p <= 34
+      ? "Yes, You found me!!"
+      : p >= 67
+      ? "No, thats Godswill"
+      : "No, thats Godsvictory";
+}
+bannerPic.addEventListener("mouseenter", () => {
+  isMe(posX);
+});
+window.addEventListener("mousemove", (e) => {
+  posX = e.clientX;
+  posY = e.clientY;
+  isMe(posX);
+});
 
-console.log("Welcome to the new project created with startease");
-console.log(
-  "Project generated with startease - @ https://startease.vercel.app",
-);
+console.log(posX, posY);
