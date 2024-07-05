e6980fbbdde517f666bca5ed51c2326536e56823
Solomon Adzape <adzapesolomon@gmail.com>
2024-07-05 08:44:09 +0100

    chore: Implemented timing function

--- hng-task-1/index.js
+++ hng-task-1/index.js
@@ -1,8 +1,12 @@
 const bannerPic = document.getElementById("banner-pic");
+const timeDisplayEl = document.getElementById("time");
+const dateDisplayEl = document.getElementById("date");
+
 const p1 = bannerPic.offsetLeft;
 const p2 = bannerPic.offsetRight;
 const width = bannerPic.width;
 let posX, posY;
+
 function isMe(x) {
   let p = ((x - p1) / width) * 100;
   bannerPic.title =
@@ -12,13 +16,52 @@ function isMe(x) {
       ? "No, thats Godswill"
       : "No, thats Godsvictory";
 }
+
 bannerPic.addEventListener("mouseenter", () => {
   isMe(posX);
 });
+
 window.addEventListener("mousemove", (e) => {
   posX = e.clientX;
   posY = e.clientY;
   isMe(posX);
 });
 
-console.log(posX, posY);
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function displayDateTime() {
+  setInterval(() => {
+    const date = new Date();
+    const curUTCHour = date.getUTCHours(),
+      curUTCMinute = date.getUTCMinutes(),
+      curUTCSecond = date.getUTCSeconds(),
+      curUTCDay = date.getUTCDay(),
+      curUTCMonth = date.getUTCMonth(),
+      curUTCYear = date.getUTCFullYear();
+
+    const curTime = `${curUTCHour < 10 ? "0" : ""}${curUTCHour}:${
+      curUTCMinute < 10 ? "0" : ""
+    }${curUTCMinute}:${curUTCSecond < 10 ? "0" : ""}${curUTCSecond}`;
+
+    const curDate = `${curUTCDay} ${months[curUTCMonth - 1]}, ${curUTCYear}`;
+    document.querySelector('.year').textContent = curUTCYear;
+
+    dateDisplayEl.textContent = curDate;
+    timeDisplayEl.textContent = curTime;
+  }, 1000);
+}
+
+displayDateTime();
